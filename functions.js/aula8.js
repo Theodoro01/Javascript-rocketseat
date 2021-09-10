a59889b907f8ad9a5cc5f0bbe4c41809b2fb49ce
Theodoro01 <theodorovieira01@gmail.com>
2021-09-10 17:48:44 -0300

    feat: Rocketseat_JS

--- functions.js/aula8.js
+++ functions.js/aula8.js
@@ -1,8 +1,28 @@
 //Callback function
 
 /*      
-    Não entendi essa aula.
-    
-    Pesquisar sobre callback ou perguntar para o Guilheme .
+    Uma callback function é uma função que está
+    sendo passada para outra função como parâmetro.
         
 */
+
+function sayMyName(name) {
+    console.log("antes da callback!");
+
+
+    // Como se extivesse acontecendo isso
+    
+    // function name() {
+    //     console.log("Estou em uma callback");
+    // }
+    
+    name()//callback
+    
+    console.log("depois da callback!");
+}
+
+sayMyName(
+    () => {
+        console.log("Estou em uma callback");
+    }
+)
---
+++ functions.js/aula9.js
@@ -0,0 +1,19 @@
+/* 
+    Function() constructor
+
+    * Expressão new 
+    * Criar um novo objeto
+    * this keyword
+
+*/
+
+function Person(name){
+    this.name = name;
+    this.walk = function(){
+        return this.name + " andando"
+    }
+}
+const mayk = new Person("Mayk");
+const joao = new Person("João");
+console.log(mayk.walk());
+console.log(joao.walk());
